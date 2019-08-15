8156d47b7943fc1338e65bae9ad026e6bc54f54d
Shylcok <1198509739@qq.com>
2019-08-15 16:18:14 +0800

    add new py

---
+++ src/matplotlib_learning/draw_figures.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: python3.7
+@author: JYFelt
+@license: Apache Licence 
+@contact: JYFelt@163.com
+@site: https://blog.csdn.net/weixin_38034182
+@software: PyCharm
+@file: draw_all_kinds_of_img.py
+@time: 2019/8/15 15:43
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 普通图
+n = 256
+X = np.linspace(-np.pi, np.pi, n, endpoint=True)
+Y = np.sin(2 * X)
+
+plt.plot(X, Y + 1, color='blue', alpha=1.00)
+plt.plot(X, Y - 1, color='blue', alpha=1.00)
+
+plt.show()
